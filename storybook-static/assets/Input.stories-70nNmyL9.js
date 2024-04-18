71e8c7de0f7fc2f07af4f87265c6b8503aba9ddc
Hyelim Choi <HyelimChoi01@gmail.com>
2024-04-18 17:08:14 +0900

    build: upstream 변경사항 스토리북 재배포

---
+++ storybook-static/assets/Input.stories-70nNmyL9.js
@@ -0,0 +1,9 @@
+import{I as a,a as c}from"./Input-BrXGgI-Q.js";import{a as t}from"./chunk-MZXVCX43-DWuJqIWT.js";import"./jsx-runtime-BnIj46N_.js";import"./index-CsdIBAqE.js";import"./system-DOfS-soT.js";import"./v4-D8aEg3BZ.js";const k={title:"Input",component:a},e=0,m=c.CARD_NUMBER,p=t("handleInput"),d=t("setErrorMessages"),o={args:{info:m.inputInfo[e],index:e,handleInput:p,isError:!1,handleErrorMessage:d}};var n,r,s;o.parameters={...o.parameters,docs:{...(n=o.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    info: mockInfo.inputInfo[mockIndex],
+    index: mockIndex,
+    handleInput: mockHandleInput,
+    isError: false,
+    handleErrorMessage: mockSetErrorMessages
+  }
+}`,...(s=(r=o.parameters)==null?void 0:r.docs)==null?void 0:s.source}}};const g=["Default"];export{o as Default,g as __namedExportsOrder,k as default};
